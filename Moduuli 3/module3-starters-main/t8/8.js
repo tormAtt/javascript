eaf767db3201f6338929c26d02a4e775eee6d193
tormAtt <attetorma@gmail.com>
2023-03-31 15:02:14 +0300

    Tehtävä 8 moduuli 3

---
+++ Moduuli 3/module3-starters-main/t8/8.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const button = document.getElementById("start");
+
+function calculate(evt) {
+  const choice = document.getElementById("operation").value;
+  console.log(choice)
+  const num1 = parseFloat(document.querySelector("#num1").value);
+  const num2 = parseFloat(document.querySelector("#num2").value);
+  let answer = 0;
+  switch(choice) {
+    case "add":
+      answer = num1 + num2;
+      document.querySelector("#result").innerHTML = answer.toString();
+      break;
+    case "sub":
+      answer = num1 - num2;
+      document.querySelector("#result").innerHTML = answer.toString();
+      break;
+    case "multi":
+      answer = num1 * num2;
+      document.querySelector("#result").innerHTML = answer.toString();
+      break;
+    case "div":
+      answer = num1 / num2;
+      document.querySelector("#result").innerHTML = answer.toString();
+      break;
+  }
+}
+
+button.addEventListener("click", calculate)
+
