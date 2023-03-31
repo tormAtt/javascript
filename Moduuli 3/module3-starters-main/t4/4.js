5e687587a47dcbb52c3adc24d1b2be7d9ece32a4
tormAtt <attetorma@gmail.com>
2023-03-31 14:00:29 +0300

    Tehtävä 4 moduuli 3

--- Moduuli 3/module3-starters-main/t4/4.js
+++ Moduuli 3/module3-starters-main/t4/4.js
@@ -13,3 +13,13 @@ const students = [
     id: '5423679',
   },
 ];
+
+const lista = document.querySelector("#target");
+
+for(let i = 0; i < students.length; i++){
+  const option = document.createElement("option");
+  const nimi = document.createTextNode(students[i].name);
+  option.appendChild(nimi);
+  lista.appendChild(option);
+  lista.getElementsByTagName("option")[i].value = students[i].id;
+}
