698ddf4c9884bd2e45fa982d1e8e3adea0d065f0
tormAtt <attetorma@gmail.com>
2023-03-31 13:51:50 +0300

    Tehtävä 3 moduuli 3

--- Moduuli 3/module3-starters-main/t3/3.js
+++ Moduuli 3/module3-starters-main/t3/3.js
@@ -1,2 +1,10 @@
 'use strict';
 const names = ['John', 'Paul', 'Jones'];
+
+let nameList = "";
+
+for(let i = 0; i < names.length; i++){
+  nameList += `<li>${names[i]}</li>`
+}
+
+document.querySelector("#target").innerHTML = nameList;
