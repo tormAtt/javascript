0e3384102fc0f5f8204e723f70a04ab9a6f5ab4f
tormAtt <attetorma@gmail.com>
2023-04-01 14:49:49 +0300

    Tehtävä 11 moduuli 3

---
+++ Moduuli 3/module3-starters-main/t11/11.js
@@ -0,0 +1,151 @@
+'use strict';
+const picArray = [
+  {
+    title: 'Title 1',
+    caption: 'Caption 1',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sodales enim eget leo condimentum vulputate. Sed lacinia consectetur fermentum. Vestibulum lobortis purus id nisi mattis posuere. Praesent sagittis justo quis nibh ullamcorper, eget elementum lorem consectetur. Pellentesque eu consequat justo, eu sodales eros.',
+    image: {
+      large: 'img/pic1.jpg',
+      medium: 'thumbnails/pic1.jpg',
+    },
+  },
+  {
+    title: 'Title 2',
+    caption: 'Caption 2',
+    description:
+      'Donec dignissim tincidunt nisl, non scelerisque massa pharetra ut. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Vestibulum tincidunt sapien eu ipsum tincidunt pulvinar. ',
+    image: {
+      large: 'img/pic2.jpg',
+      medium: 'thumbnails/pic2.jpg',
+    },
+  },
+  {
+    title: 'Title 3',
+    caption: 'Caption 3',
+    description:
+      'Phasellus imperdiet nunc tincidunt molestie vestibulum. Donec dictum suscipit nibh. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. ',
+    image: {
+      large: 'img/pic3.jpg',
+      medium: 'thumbnails/pic3.jpg',
+    },
+  },
+  {
+    title: 'Title 4',
+    caption: 'Caption 4',
+    description:
+      'Duis sodales enim eget leo condimentum vulputate. Sed lacinia consectetur fermentum. Vestibulum lobortis purus id nisi mattis posuere. Praesent sagittis justo quis nibh ullamcorper, eget elementum lorem consectetur. Pellentesque eu consequat justo, eu sodales eros. Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
+    image: {
+      large: 'img/pic4.jpg',
+      medium: 'thumbnails/pic4.jpg',
+    },
+  },
+  {
+    title: 'Title 5',
+    caption: 'Caption 5',
+    description:
+      'Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Vestibulum tincidunt sapien eu ipsum tincidunt pulvinar. Donec dignissim tincidunt nisl, non scelerisque massa pharetra ut. ',
+    image: {
+      large: 'img/pic5.jpg',
+      medium: 'thumbnails/pic5.jpg',
+    },
+  },
+  {
+    title: 'Title 6',
+    caption: 'Caption 6',
+    description:
+      'Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Phasellus imperdiet nunc tincidunt molestie vestibulum. Donec dictum suscipit nibh.',
+    image: {
+      large: 'img/pic6.jpg',
+      medium: 'thumbnails/pic6.jpg',
+    },
+  },
+  {
+    title: 'Title 7',
+    caption: 'Caption 7',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sodales enim eget leo condimentum vulputate. Sed lacinia consectetur fermentum. Vestibulum lobortis purus id nisi mattis posuere. Praesent sagittis justo quis nibh ullamcorper, eget elementum lorem consectetur. Pellentesque eu consequat justo, eu sodales eros.',
+    image: {
+      large: 'img/pic7.jpg',
+      medium: 'thumbnails/pic7.jpg',
+    },
+  },
+  {
+    title: 'Title 8',
+    caption: 'Caption 8',
+    description:
+      'Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Vestibulum tincidunt sapien eu ipsum tincidunt pulvinar. Donec dignissim tincidunt nisl, non scelerisque massa pharetra ut. Sed vel velit ante. Aenean quis viverra magna. ',
+    image: {
+      large: 'img/pic8.jpg',
+      medium: 'thumbnails/pic8.jpg',
+    },
+  },
+  {
+    title: 'Title 9',
+    caption: 'Caption 9',
+    description:
+      'Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Phasellus imperdiet nunc tincidunt molestie vestibulum. Donec dictum suscipit nibh. Sed vel velit ante. Aenean quis viverra magna. ',
+    image: {
+      large: 'img/pic9.jpg',
+      medium: 'thumbnails/pic9.jpg',
+    },
+  },
+];
+
+// add your code here
+
+const section = document.querySelector("#pictures");
+for(let i = 0; i < picArray.length; i++){
+  const article = document.createElement("article");
+  article.className = "card";
+
+  const header = document.createElement("h2");
+  const hText = document.createTextNode(picArray[i].title);
+  header.appendChild(hText);
+  article.appendChild(header);
+
+  const figure = document.createElement("figure");
+  const image = document.createElement("img");
+  image.src = picArray[i].image.medium;
+  image.alt = picArray[i].title;
+  const figcaption = document.createElement("figcaption");
+  const figText = document.createTextNode(picArray[i].caption);
+  figcaption.appendChild(figText);
+  figure.appendChild(image);
+  figure.appendChild(figcaption);
+  article.appendChild(figure);
+
+  const description = document.createElement("p");
+  const dText = document.createTextNode(picArray[i].description);
+  description.appendChild(dText);
+  article.appendChild(description);
+
+  section.appendChild(article);
+}
+
+const dialog = document.getElementsByTagName("dialog")[0];
+for(let i = 0; i < section.getElementsByTagName("article").length; i++) {
+  const article = section.getElementsByTagName("article")[i];
+  article.addEventListener("click", () => {openModal(dialog, i)});
+}
+
+function getLargeImage(i){
+  for(let j = 0; j < picArray.length; j++) {
+    if(picArray[i].title === picArray[j].title) {
+      return picArray[j].image.large;
+    }
+  }
+}
+
+function closeModal(dialog) {
+  dialog.close();
+}
+function openModal(dialog, i){
+  console.log(i);
+  dialog.getElementsByTagName("img")[0].src = getLargeImage(i);
+  dialog.getElementsByTagName("img")[0].alt = picArray[i].title;
+  dialog.showModal();
+  const span = dialog.getElementsByTagName("span")[0];
+  span.addEventListener("click", () => {closeModal(dialog)});
+}
+
