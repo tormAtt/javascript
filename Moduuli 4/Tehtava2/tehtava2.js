8033718fab1cc98af91a1753392d00cdd414b7c5
tormAtt <attetorma@gmail.com>
2023-04-02 14:15:32 +0300

    Tehtävä 2 moduuli 4

---
+++ Moduuli 4/Tehtava2/tehtava2.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const form = document.getElementById("form");
+form.addEventListener("submit", getInfo);
+async function getInfo(evt) {
+  evt.preventDefault();
+  const haku = document.querySelector("input[name=q]").value;
+  console.log(haku)
+  try {
+    const response = await fetch(`https://api.tvmaze.com/search/shows?q=${haku}`);
+    const json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
